abf407d98e7fbbf2b0413a968588e398025fb610
oshagina6 <shaginao04@gmail.com>
2022-02-19 22:22:00 +0100

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно
+//из MDN WEB DOCS
+const getRandomIntInclusive=(min, max)=>{
+  if(max>min||(max-min)>=0){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  }
+  console.log('Проверьте min и max');
+};
+//Функция для проверки максимальной длины строки
+
+const controlMaxLength=(frase,STRING_MAXLENGTH)=>{
+  if(frase.length<STRING_MAXLENGTH){
+    return true;
+  } return false;
+};
+
+getRandomIntInclusive(0,10);
+controlMaxLength('Hello',140);
