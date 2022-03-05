ac470d8f5e5fc012beec3595b8ee2d7d7fc14017
oshagina6 <shaginao04@gmail.com>
2022-03-05 16:50:38 +0100

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,15 +1,61 @@
+const SIMILAR_DESCRIPTION_COUNT=25;
+const NAMES=[
+  'Ольга',
+  'Илья',
+  'Катерина',
+  'Мария',
+  'Иван',
+  'Тарас',
+];
+const MESSAGES=[
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+const DESCRIPTIONS=[
+  'Море море',
+  'Буду помнить',
+  'Счастливые моменты',
+  'Было тепло',
+  'Май 2020',
+  'На природе',
+  'С друзьями',
+  'Тихая природа',
+];
+
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
-//из MDN WEB DOCS
-const getRandomIntInclusive = (min, max) => {
-  if(max > min){
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-  }
-  return false;
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
 //Функция для проверки максимальной длины строки
 const compareMaxLengthWithValue = (phrase, value) => phrase.length <= value;
-
-getRandomIntInclusive(0,10);
 compareMaxLengthWithValue('Hello', 140);
+
+//функция получения случайного элемента из массива
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length-1)];
+
+
+const createDescriptionPhoto = () => ({
+  id: getRandomPositiveInteger(1, 25),
+  url: `photos/${getRandomPositiveInteger(1, 25)}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomPositiveInteger(15, 200),
+  comments: {
+    id: getRandomPositiveInteger(1, 1000),
+    avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+    message: getRandomArrayElement(MESSAGES),
+    name: getRandomArrayElement(NAMES),
+  },
+});
+
+//функция для создания массива из 25 сгенерированных объектов. Каждый объект массива — описание фотографии, опубликованной пользователем.
+const similarDescriptionPhotos=Array.from({length: SIMILAR_DESCRIPTION_COUNT}, createDescriptionPhoto);
+
+// eslint-disable-next-line no-console
+console.log(similarDescriptionPhotos);
